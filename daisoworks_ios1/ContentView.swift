d4052c234438b416197644e7aff8e4ed0be23906
nicemogly <newmogly@naver.com>
2024-09-25 16:04:07 -0700

    test

--- daisoworks_ios1/ContentView.swift
+++ daisoworks_ios1/ContentView.swift
@@ -1,5 +1,4 @@
 //
-//  ContentView.swift
 //  daisoworks_ios1
 //
 //  Created by AD2201016P02 on 9/25/24.
@@ -8,20 +7,106 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var id: String = ""
+    @State private var password: String = ""
+    @State private var isCheck = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("안녕하세요\n0000입니다.")
+                    .bold(true)
+                    .font(.system(size: 25))
+                     .multilineTextAlignment(.leading)
+                     .padding(.bottom, 10)
+                
+                Spacer()
+            }.frame(width: 280)
+            
+            HStack {
+                Text("회원 서비스 이용을 위해 로그인 해주세요.")
+                        .bold(true)
+                        .font(.system(size: 16))
+                        .multilineTextAlignment(.leading)
+                        .padding(.bottom, 20)
+                
+                Spacer()
+            }.frame(width: 280)
+            
+            // 이메일 입력
+            TextField(
+                "Email",
+                text: $id
+            )
+            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 0))
+            .textInputAutocapitalization(.never)
+            .background(Color(red: 249/255, green: 245/255, blue: 244/255))
+            .frame(width: 280, height: 40)
+            .cornerRadius(7)
+
+            // 비밀번호 입력
+            SecureField(
+                "Password",
+                text: $password
+            )
+            .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 0))
+            .textInputAutocapitalization(.never)
+            .background(Color(red: 249/255, green: 245/255, blue: 244/255))
+            .frame(width: 280, height: 40)
+            .cornerRadius(7)
+
+
+            // 로그인 버튼
+            Button(action: {}, label: {
+                Text("Sign in")
+                    .foregroundColor(.white)
+                    .frame(width: 280, height: 40)
+                    .background(Color.blue)
+                    .cornerRadius(5)
+            })
+            HStack {
+                checkBoxView(isCheck: true, checkSize: 17, backgroundSize: 20)
+                Text("로그인 상태 유지")
+                Spacer()
+            }.frame(width: 280)
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct checkBoxView: View {
+    @State var isCheck: Bool
+    let checkColor: Color
+    let backgroungColor: Color
+    let checkSize : CGFloat
+    let conerRadius: CGFloat
+    let backgroundSize: CGFloat
     
+    init(isCheck: Bool, checkColor: Color = Color.white, backgroundColor: Color = Color(.lightGray), checkSize: CGFloat = 12.5, conerRadius: CGFloat = 5, backgroundSize: CGFloat = 15) {
+        self.isCheck = isCheck
+        self.checkColor = checkColor
+        self.backgroungColor = backgroundColor
+        self.checkSize = checkSize
+        self.conerRadius = conerRadius
+        self.backgroundSize = backgroundSize
+    }
     
-    
+    var body: some View {
+        Button(action: {isCheck = !isCheck}, label: {
+            if(isCheck) {
+                ZStack(alignment: .center, content: {
+                    RoundedRectangle(
+                        cornerRadius: conerRadius)
+                        .fill(backgroungColor)
+                        .frame(width: backgroundSize, height: backgroundSize)
+                    Image(systemName: "checkmark").foregroundStyle(checkColor).font(.system(size: checkSize))
+                })
+            } else {
+                RoundedRectangle(
+                    cornerRadius: conerRadius)
+                    .strokeBorder(lineWidth: 0.2)
+                    .foregroundStyle(Color.black)
+                    .frame(width: backgroundSize, height: backgroundSize)
+            }
+        })
+    }
 }
