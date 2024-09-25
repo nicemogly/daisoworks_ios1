821ddc359e404ac83e2c227d2a2347c9118dc0fd
nicemogly <newmogly@naver.com>
2024-09-25 14:54:11 -0700

    Initial Commit

---
+++ daisoworks_ios1/daisoworks_ios1App.swift
@@ -0,0 +1,17 @@
+//
+//  daisoworks_ios1App.swift
+//  daisoworks_ios1
+//
+//  Created by AD2201016P02 on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct daisoworks_ios1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ daisoworks_ios1Tests/daisoworks_ios1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  daisoworks_ios1Tests.swift
+//  daisoworks_ios1Tests
+//
+//  Created by AD2201016P02 on 9/25/24.
+//
+
+import Testing
+@testable import daisoworks_ios1
+
+struct daisoworks_ios1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
